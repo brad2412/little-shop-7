b0eb7e9704f0b756276f3dd8462e0de549159e65
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-05 17:41:55 -0600

    add update method

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -8,4 +8,14 @@ class Merchants::ItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @item= Item.find(params[:id])
   end
+
+  def edit 
+    show
+  end
+
+  def update
+    show
+    @item.update(name: params[:name], unit_price: params[:unit_price], description: params[:description])
+    redirect_to merchant_item_path(@merchant, @item)
+  end
 end
