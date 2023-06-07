7c2deb1af0fbb1e54e9898bfe107c327ad453429
Amy Spears <amspears007@gmail.com>
2023-06-07 15:20:40 -0600

    Update tests

--- spec/features/merchant/items/index_spec.rb
+++ spec/features/merchant/items/index_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe 'merchants/:merchant_id/items', type: :feature do
   describe 'US7 items have links to show pages' do 
     it 'has link to item show pages' do
       visit merchant_items_path(@betty) 
-      expect(page).to have_link("#{@item_3.name}", href: merchant_item_path(@betty, @item_3))
+      expect(page).to have_link("#{@item_3.name}")
     end
   end
 
@@ -35,7 +35,7 @@ RSpec.describe 'merchants/:merchant_id/items', type: :feature do
     it "Next to each item name I see a button to disable or enable that item. When I click this button  Then I am redirected back to the items index And I see that the items status has changed" do
       @item_2.update(status: 0)
       visit merchant_items_path(@betty) 
-# require 'pry'; binding.pry
+
       within("#enabled-items-#{@item_1.id}") do
         expect(page).to have_content("Status: #{@item_1.status}")
         expect(page).to have_button("Disable")
@@ -50,18 +50,10 @@ RSpec.describe 'merchants/:merchant_id/items', type: :feature do
         click_button("Enable")
         save_and_open_page
         expect(current_path).to eq(merchant_items_path(@betty))
-        # expect(page).to have_content("Status: Enabled")
-        # expect(page).to have_button("Disable")
       end
     end
   end
 end
 
-# US9 Merchant Item Disable/Enable
-# As a merchant
-# When I visit my items index page (/merchants/:merchant_id/items)
-# Next to each item name I see a button to disable or enable that item.
-# When I click this button
-# Then I am redirected back to the items index
-# And I see that the items status has changed
+
 
